57325ce3c88f02e3aeb694156be9a5fa28b47b4f
Gentrit Abazi <gentritabazi01@gmail.com>
2020-10-16 13:35:40 +0200

    Added logic for Created.

--- src/Traits/ModelEventLogger.php
+++ src/Traits/ModelEventLogger.php
@@ -2,6 +2,8 @@
 
 namespace one2tek\laralog\Traits;
 
+use one2tek\laralog\Models\LaraLog;
+
 trait ModelEventLogger
 {
     /**
@@ -9,6 +11,18 @@ trait ModelEventLogger
      */
     protected static function bootModelEventLogger()
     {
-        //
+        // Created
+        self::created(function ($model) {
+            $data = [
+                'event_type' => 'created',
+                'subject_type' => get_class($model),
+                'subject_id' => $model->id,
+                'causer_type' => get_class(auth()->user()),
+                'causer_id' => auth()->user()->id,
+                'properties' => ['new_attributes' => $model->getAttributes()]
+            ];
+
+            (new LaraLog)->create($data);
+        });
     }
 }
