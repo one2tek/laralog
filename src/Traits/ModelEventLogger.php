098468dfc1037ceb00aa499940999cc94d3a7615
Gentrit Abazi <gentritabazi@Gentrit-Abazi.local>
2020-10-16 10:28:06 +0200

    Added base.

---
+++ src/Traits/ModelEventLogger.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace one2tek\larapi\Traits;
+
+trait ModelEventLogger
+{
+    /**
+     * Automatically boot with Model, and register Events handler.
+     */
+    protected static function bootModelEventLogger()
+    {
+        //
+    }
+}
