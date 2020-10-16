e47dd5faf6d1db646564594fd128c13781ec7ef8
Gentrit Abazi <gentritabazi01@gmail.com>
2020-10-16 11:45:33 +0200

    Added logs config.

---
+++ config/logs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    
+    /*
+     * This is the name of the table that will be created by the migration and
+     * used by the Log model shipped with this package.
+     */
+    'table_name' => 'activity_log',
+
+    /*
+     * This is the database connection that will be used by the migration and
+     * the Activity model shipped with this package.
+     */
+    'database_connection' => 'mysql'
+];
