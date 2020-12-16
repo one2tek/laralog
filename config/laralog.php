a9f91d61ca55324691c004ee07938114851cfbc4
Gentrit Abazi <gentritabazi01@gmail.com>
2020-12-16 13:45:37 +0100

    Added possibility to disable logs in specific environments.

--- config/laralog.php
+++ config/laralog.php
@@ -12,5 +12,12 @@ return [
      * This is the database connection that will be used by the migration and
      * the LaraLog model shipped with this package.
      */
-    'database_connection' => 'mysql'
+    'database_connection' => 'mysql',
+
+
+    /*
+     * If you want to disable logs in specific environments this is
+     * the exactly place.
+     */
+    'disable_on_environments' => ['local'],
 ];
